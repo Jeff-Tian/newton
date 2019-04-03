eeacd4a919d28b2566b572cfe4c936bdc3b52dcf
Jeff Tian <jie.tian@unicareer.com>
2019-04-03 12:45:07 +0800

    Test x**2 - 1

--- src/test/newton.test.ts
+++ src/test/newton.test.ts
@@ -26,3 +26,16 @@ describe('f(x) = -x', () => {
     assert(Newton.solve(f, f_, -50, 100) < delta)
   })
 })
+
+describe('f(x) = x**2-1', () => {
+  const f = x => x ** 2 - 1
+  const f_ = x => 2 * x
+
+  it('x = 1', () => {
+    assert(Newton.solve(f, f_, 0, 100) - 1 < delta)
+  })
+
+  it('x = -1', () => {
+    assert(Newton.solve(f, f_, -100, 0) + 1 < delta)
+  })
+})
